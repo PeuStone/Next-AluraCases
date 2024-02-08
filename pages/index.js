fbdbe92f41a4c20d3779afbc131395a4bfa18570
PeuStone <pedrohmotaaa@gmail.com>
2024-02-08 10:42:46 -0300

    Adicionando a biblioteca fontAwesome ao projeto, criação de Temas e criação da Pagina inicial juntamente com o Footer

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,34 @@
-import Link from "next/link"
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children, as }) {
-    const Tag = as;
-    return (
-        <>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                }
-            `}</style>
-        </>
-    )
-}
+export default HomeScreen
+// import Link from "next/link"
+// import Footer from "../src/components/patterns/Footer";
 
-export default function HomePage() {
-    return (
-        <div>
-            <Title as='h1'>Alura Cases - Home page</Title>
-            <Link href="/faq">
-                Ir para a página do FAQ
-            </Link>
-        </div>
-    )
-}
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+//     )
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as='h1'>Alura Cases - Home page</Title>
+//             <Link href="/faq">
+//                 Ir para a página do FAQ
+//             </Link>
+//             <Footer />
+//         </div>
+//     )
+// }
