f42e7d847da105c6a3e0155088c146245a24c70c
PeuStone <pedrohmotaaa@gmail.com>
2024-02-10 10:18:31 -0300

    Edição da pagina de FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,6 @@
-import Head from "next/head";
-import Link from "next/link";
+import FAQScreen from '../src/screens/FAQScreen'
+
+export default FAQScreen;
 
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -16,30 +17,4 @@ export async function getStaticProps() {
             faq,
         },
     };
-}
-
-export default function FAQPage({ faq }) {
-    console.log(faq);
-
-    return (
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Cases - Página de perguntas FAQ</h1>
-            <Link href='/'>
-                Ir para a home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
 }
